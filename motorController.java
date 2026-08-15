ac905646e9ba7ceb60a564fe7eb9947f547e89b7
Will Ahrens <bf5b9bs55j@privaterelay.appleid.com>
2026-08-15 13:43:56 -0500

    Create motorController.java
    This file takes the location that you want to get to and provides step by step changes to the position of the robot to get it to that point.

---
+++ motorController.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+public class motorcontroller {
+    robothardwaremanager robot;
+    public char xchange;
+    public char ychange;
+    public double headingchange;
+    private double xpose;
+    private double ypose;
+    private double thetapose;
+    public void motorcontroller() {
+
+    }
+    public void Driveto(double xpose,double ypose,double thetapose) {
+
+    }
+
+}
