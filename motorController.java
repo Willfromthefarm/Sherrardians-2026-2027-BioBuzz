24b78bdb30d099cd5d888a376aefde2adaad710e
Will Ahrens <bf5b9bs55j@privaterelay.appleid.com>
2026-08-21 13:51:52 -0500

    Update motorController.java
    added some framework to go off of

--- motorController.java
+++ motorController.java
@@ -1,17 +1,32 @@
 package org.firstinspires.ftc.teamcode.auto;
 
 public class motorcontroller {
-    robothardwaremanager robot;
+    robothardwaremanager mCRobot;
+    public motorcontroller(robothardwaremanager robot) {
+        mCRobot = robot;
+    }
     public char xchange;
     public char ychange;
     public double headingchange;
     private double xpose;
     private double ypose;
     private double thetapose;
-    public void motorcontroller() {
+    public void turnto(double thetapose) {
 
     }
-    public void Driveto(double xpose,double ypose,double thetapose) {
+    public void forwardTo(double xpose) {
+
+    }
+    public void strafeTo(double ypose) {
+
+    }
+    public void pathTo(double xpose,double ypose,double thetapose) {
+        //path generator here
+        //- bezier curve gen here
+        //- direct path gen here
+        //tight turn detector here
+        //colision correction here
+        //motion profiling here
 
     }
 
