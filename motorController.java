8f02bba720de6ecce56462df88e064e66295abf4
Will Ahrens <bf5b9bs55j@privaterelay.appleid.com>
2026-08-24 17:52:00 -0500

    Update motorController.java
    Updated the controller to have the base for the pathTo function

--- motorController.java
+++ motorController.java
@@ -1,16 +1,40 @@
 package org.firstinspires.ftc.teamcode.auto;
 
+import org.firstinspires.ftc.teamcode.Beziergen;
+import org.firstinspires.ftc.teamcode.utilityclass;
+
 public class motorcontroller {
     robothardwaremanager mCRobot;
     public motorcontroller(robothardwaremanager robot) {
         mCRobot = robot;
     }
+    private Beziergen bezier;
+    utilityclass utility = new utilityclass();
+    utilityclass.coordinates firstpoint = utility.new coordinates(0,0);
+    utilityclass.coordinates nextpoint = utility.new coordinates(0,0);
+
     public char xchange;
+    
+    public boolean curveEnabled = false;
+    
     public char ychange;
+    
     public double headingchange;
+    
     private double xpose;
+    
     private double ypose;
+    
     private double thetapose;
+    
+    private double firstX;
+    
+    private double firstY;
+    
+    private double nextX;
+    
+    private double nextY;
+    
     public void turnto(double thetapose) {
 
     }
@@ -21,8 +45,27 @@ public class motorcontroller {
 
     }
     public void pathTo(double xpose,double ypose,double thetapose) {
+        localizationhub localization = new localizationhub(mCRobot);
+        double headingDifference = Math.abs(
+                ((thetapose - localization.currentheading + 180) % 360) - 180
+        );
         //path generator here
-        //- bezier curve gen here
+        if(headingDifference<90){
+            curveEnabled = true;
+            bezier = new Beziergen(xpose,ypose,thetapose); //bezier gen
+        }else{
+            curveEnabled = false;
+        }
+
+        if(curveEnabled = true){
+            firstX = firstpoint.getXCoord();
+            firstY = firstpoint.getYCoord();
+            nextpoint = bezier.nextPoint();
+        }else{
+
+        }
+
+
         //- direct path gen here
         //tight turn detector here
         //colision correction here
