ef0c403c2cff806b34fe1ee8d2a59eb89f534dd6
SherrardTigers <143823445+SherrardTigers@users.noreply.github.com>
2026-08-24 13:18:38 -0500

    add 4 motor variables to codebase

--- command center.java
+++ command center.java
@@ -2,13 +2,18 @@ package org.firstinspires.ftc.teamcode.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 // this code demonstrates the advanced decision making process of the Sherrardian Autonomous
+
+// can we call it Authonomas Thomas
 
     @Autonomous(name="CommandAuto", group="Robot")
     //@Disabled
     public class commandcenter extends LinearOpMode {
-
+        //so anyways, we set up the motors
+        private DcMotor FR, FL, BR, BL //front and back left and right, could change if the robot isn't a 4-wheeler for some reason or another
 
         robothardwaremanager robot = new robothardwaremanager();
         @Override
