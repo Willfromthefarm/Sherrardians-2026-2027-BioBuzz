16099ccf316cd4bfe9d9fb19363c1cb4a847d50b
Will Ahrens <bf5b9bs55j@privaterelay.appleid.com>
2026-09-22 17:41:53 -0500

    Update utility class.java
    Edited the current hardware map to include the current devices planned

--- utility class.java
+++ utility class.java
@@ -1,58 +1,64 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.auto;
 
-public class utilityclass {
-    class coordinates{
-        private double xCoord;
-        private double yCoord;
-        public coordinates(double xCoord,double yCoord){
-            this.xCoord = xCoord;
-            this.yCoord = yCoord;
-        }
-        public double getXCoord(){
-            return xCoord;
-        }
-        public double getYCoord(){
-            return yCoord;
-        }
-    }
-    class Queue{
-        private int maxSize;
-        private coordinates[ ] queArray;
-        private int front;
-        private int rear;
-        private int nItems;
-        //-----------------------------------------
-        public Queue(int s){
-            maxSize = s;
-            queArray = new coordinates[maxSize];
-            front = 0;
-            rear = -1;
-            nItems = 0;
-        }
-        //-----------------------------------------
-        public void insert(coordinates coord){
-            if(rear == maxSize-1)
-                rear = -1;
-            queArray[++rear] = coord;
-            nItems++;
-        }
-        //------------------------------------------
-        public coordinates remove(){
-            coordinates temp = queArray[front++];
-            if(front == maxSize)
-                front = 0;
-            nItems--;
-            return temp;
-        }
-        //-----------------------------------------
-        public coordinates peekFront(){
-            return queArray[front];
-        }
-        public boolean isFull(){
-            return (nItems==maxSize);
-        }
-        public int size(){
-            return nItems;
-        }
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+public class robothardwaremanager {
+    private float colorGain=2;
+    // Motors
+    public DcMotor frontLeft, frontRight, backLeft, backRight;
+    public DcMotor speedy2;
+    public DcMotor intake;
+    public DcMotor speedy1;
+   // public DcMotor intake1;
+    public Servo servolaunch;
+    public Servo storageservo;
+    public Servo turretservo;
+    // Pinpoint odometry
+    public GoBildaPinpointDriver odo;
+    public NormalizedColorSensor ballSensor;
+    
+
+    public void init(HardwareMap hardwareMap) {
+        // Initialize motors - motor names from your config
+        frontLeft = hardwareMap.get(DcMotor.class, "motor 1");
+        frontRight = hardwareMap.get(DcMotor.class, "motor 3");
+        backLeft = hardwareMap.get(DcMotor.class, "motor 2");
+        backRight = hardwareMap.get(DcMotor.class, "motor 4");
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        speedy1 = hardwareMap.get(DcMotor.class, "upperlaunch");
+        speedy2 = hardwareMap.get(DcMotor.class, "lowerlaunch");
+        servolaunch = hardwareMap.get(Servo.class, "servolaunch");
+        storageservo = hardwareMap.get(Servo.class, "storageservo");
+        turretservo = hardwareMap.get(Servo.class, "turretservo");
+        ballSensor = hardwareMap.get(NormalizedColorSensor.class, "ballSensor");
+
+        // Set motor directions - ADJUST IF ROBOT MOVES BACKWARDS
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        // Set zero power behavior
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //set the color sensor settings
+        ballSensor.setGain(colorGain);
+
+        // Initialize Pinpoint - ADJUST NAME TO MATCH YOUR CONFIG
+        odo = hardwareMap.get(GoBildaPinpointDriver.class, "odo");
+        odo.setOffsets(-84.0, -168.0, DistanceUnit.MM); // ADJUST TO YOUR ROBOT'S OFFSETS (mm)
+        odo.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
+        odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD,
+                GoBildaPinpointDriver.EncoderDirection.FORWARD);
+        odo.resetPosAndIMU();
     }
 }
